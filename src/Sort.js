c318303e8e1c01e6470b6df04f0b9468c81a9c61
pollyj <55009539+pollyj@users.noreply.github.com>
2020-04-13 17:45:03 +0900

    Recloned files to fix nodemon issue

--- src/Sort.js
+++ src/Sort.js
@@ -1,9 +1,23 @@
-class SomeKindOfSort {
+class BubbleSort {
   constructor(array) {
     this.array = array;
   }
-  sort() {
-    console.log("sort!");
+  sort(array) {
+    array = this.array;
+    let sortedNum = 0;
+    for (let j = 0; j <= array.length; j++) {
+      for (let i = 0; i <= array.length - sortedNum; i++) {
+        if (array[i] > array[i + 1]) {
+          this.swap(array, i, i + 1);
+        }
+      }
+      sortedNum += 1;
+    }
+    return array;
+  }
+
+  swap(array, index1, index2) {
+    [array[index1], array[index2]] = [array[index2], array[index1]];
   }
 
   returnValue(value) {
@@ -11,4 +25,32 @@ class SomeKindOfSort {
   }
 }
 
-module.exports = SomeKindOfSort;
+const arrayTest = [
+  1,
+  7,
+  3,
+  20,
+  -4,
+  15,
+  105,
+  57,
+  94,
+  5,
+  72,
+  26,
+  0,
+  173,
+  4,
+  75,
+  46,
+  35,
+  76,
+  13,
+  834,
+  95,
+  20,
+  46,
+  34,
+];
+
+module.exports = BubbleSort;
